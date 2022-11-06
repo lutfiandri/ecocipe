d1af562a5346576b9efa8122d18ef0b180341de3
lutfiandri <lutfiandri232@gmail.com>
2022-11-06 21:48:36 +0700

    feat(postgres): initiate connection to postgres

--- Ecocipe/Forms/Discover.cs
+++ Ecocipe/Forms/Discover.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Npgsql;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,49 @@ namespace Ecocipe.Forms
         public Discover()
         {
             InitializeComponent();
+        }
+
+        private NpgsqlConnection conn;
+        private string connstring = "Host=localhost;Port=5432;Username=lutfiandri;Password=glhf;Database=ecocipe";
+
+        private void Discover_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                conn = new NpgsqlConnection(connstring);
+                conn.Open();
+                Console.WriteLine("Database connected");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occured when opening postgres connection.", ex.Message);
+            }
+
+            // get all recipes on load
+            try
+            {
+                var sql = "select * from select_all_recipes()";
+                var cmd = new NpgsqlCommand(sql, conn);
+                var data = cmd.ExecuteReader();
+                // TODO: show to UI (create the UI first)
+                Console.WriteLine(data);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private void Discover_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occured when closing postgres connection.", ex.Message);
+            }
         }
     }
 }
